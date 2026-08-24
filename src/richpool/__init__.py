3217b4ed750f6412b53319f8058e037e11f9256e
Sahil Jhawar <jhawar@gfz.de>
2026-08-24 12:04:07 +0200

    version: bump version 0.1.3 -> 0.1.4

--- src/richpool/__init__.py
+++ src/richpool/__init__.py
@@ -8,7 +8,7 @@ from richpool.multi import MultiPool
 from richpool.pool import BasePool
 from richpool.serial import SerialPool
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 __all__ = [
     "__version__",
