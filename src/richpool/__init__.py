ad821f9cb060c26fcdf411226e72b0670922dfa9
Sahil Jhawar <sahil.jhawar448@gmail.com>
2026-09-15 21:37:38 +0200

    version: bump version 0.2.0 -> 0.2.1

--- src/richpool/__init__.py
+++ src/richpool/__init__.py
@@ -8,7 +8,7 @@ from richpool.multi import MultiPool
 from richpool.pool import BasePool
 from richpool.serial import SerialPool
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __all__ = [
     "__version__",
